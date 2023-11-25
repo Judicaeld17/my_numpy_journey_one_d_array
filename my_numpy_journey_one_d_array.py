0187c37a825f1d3e3ded960050364da16e14206d
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:27:13 +0100

    first commit

---
+++ my_numpy_journey_one_d_array.py
@@ -0,0 +1,4 @@
+# TASK : Create a function which take a standard array as parameter and returns a Numpy Array.
+import numpy as np 
+def my_numpy_journey_one_d_array(tab):
+    return np.array([tab])
